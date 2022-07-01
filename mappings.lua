47934afd3394f4b56e82eb753d55319c31e7c0bf
Hary Prihadi <phdhary@gmail.com>
2022-07-01 07:34:50 +0700

    add mapping for ZenMode and LazyGit

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,8 @@ M.telescope = {
       ["<leader>fl"] = { "<cmd> Telescope flutter commands<CR>", "   flutter tools" },
       ["<leader>fk"] = { "<cmd> lua vim.lsp.buf.formatting()<CR>", "👊 lsp force format" },
       ["<leader>ca"] = { "<cmd> CodeActionMenu<CR>", "  lsp code action menu" },
+      ["<leader>zn"] = { "<cmd> ZenMode<CR>", "🙏 zen mode" },
+      ["<leader>lg"] = { "<cmd> LazyGit<CR>", " open lazy git" },
       -- ["<leader>rr"] = { "<cmd> '%s/' . @/ . '//gc' norm <LEFT><LEFT><LEFT>'", "replace all" },
    },
 }
