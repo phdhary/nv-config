efb2363c950ed32aa1a651d0f1046716dda3f81c
Hary Prihadi <phdhary@gmail.com>
2022-07-20 10:41:56 +0700

    format indent

--- plugins/init.lua
+++ plugins/init.lua
@@ -44,9 +44,9 @@ M.user = {
    },
 
    --["simrat39/rust-tools.nvim"] = {
-     --config  = function ()
-      --require("custom.plugins.configs.rust_tools")
-     --end
+   --config  = function ()
+   --require("custom.plugins.configs.rust_tools")
+   --end
    --},
 
    ["jose-elias-alvarez/null-ls.nvim"] = {
